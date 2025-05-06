41b305227a6f73a3469aa48b192035eb15e05ed2
LevBord-wowie <bordlev7@gmail.com>
2025-05-06 09:04:15 +0000

    First code in C++

--- main.cpp
+++ main.cpp
@@ -1,1 +1,19 @@
+#include <iostream>
+#include <string>
+using namespace std;
 
+void my_print(const string &text) {
+    cout << text << endl;
+}
+
+int main() {
+    string text;
+
+    my_print("Ala ma kota");
+    my_print("Podaj text");
+    getline(cin, text);
+    string newText = "Nowy Tekst = " + text;
+    my_print(newText);
+
+    return 0;
+}
